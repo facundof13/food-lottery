46fc0cdb42f2a7f5665ab59a9378695397ca76ec
Facundo Figueroa <Facundof13@gmail.com>
2019-06-18 14:03:56 -0400

    timeout error

--- food.js
+++ food.js
@@ -1,36 +1,26 @@
 require('dotenv').config();
 
 const googleMaps = require('@google/maps').createClient({
-  key: process.env.API_KEY
+  key: "AIzaSyCoKs7KMvBcRsIt4thBeC8fmVq-c4fmkp4",
+  Promise: Promise
 });
 
 let string = "Tacos";
 let pos = [33.84217884331358, -84.10194936086585];
 
-// getNearbyFood(string, pos[0], pos[1])
-// .then(result => {
-//   console.log(result);
-// })
 
 
 // async function getNearbyFood(string, lat, long) {
-
-//   }, function(result) {
-//     console.log(result);
-//   })
-
-test();
-
-function test() {
- googleMaps.places({
-    query: 'fast food',
-    language: 'en',
-    location: [33.842, -84.101],
-    radius: 5000
-  }, response => {
+googleMaps.places({
+    query: string,
+    location: pos
+  }, function (result) {
+    console.log(result);
+  }).asPromise()
+  .then(function (response) {
     console.log(response.json.results);
-  });
-}
+  })
+// }
 
 
 
