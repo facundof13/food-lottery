fb38726f7b79a625b5616be9e7c37d5d63bbc6c4
Facundo Figueroa <Facundof13@gmail.com>
2019-06-19 15:33:35 -0400

    working api calls

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 require('dotenv').config()
 const express = require('express');
-const nearbyFood = require('./food');
+const food = require('./food');
 
 const app = express();
 app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 
 
-app.get('/api/:searchstring&:lat&:long',  (request, response) => {
+app.get('/api/:searchstring&:lat&:long', (request, response) => {
   const searchstring = request.params.searchstring;
   const lat = request.params.lat;
   const long = request.params.long;
 
-  const result = nearbyFood.getNearbyFood(searchstring, lat, long);
-  console.log(result);
-  // .then(result => {
-  //   response.send(result);
-  // })
-
+  if (searchstring !== '' && searchstring !== null)
+    if (lat !== null && long !== null) {
+      food.callPlacesAPI(searchstring, lat, long)
+        .then(res => {
+          response.send(res);
+        })
+    }
 });
 
 module.exports = app;
--- food.js
+++ food.js
@@ -1,31 +1,52 @@
 require('dotenv').config();
 
 const googleMaps = require('@google/maps').createClient({
-  key: "AIzaSyCoKs7KMvBcRsIt4thBeC8fmVq-c4fmkp4",
+  key: process.env.API_KEY,
   Promise: Promise
 });
 
-let string = "Tacos";
-let pos = [33.84217884331358, -84.10194936086585];
+// let string = "Tacos";
+// let pos = [33.84217884331358, -84.10194936086585];
+
+// callPlacesAPI(string, pos[0], pos[1])
+//   .then(response => {
+//     response.forEach((place) => {
+//       console.log(place);
+//     })
+//   });
+
+async function callPlacesAPI(place, lat, long) {
+  return new Promise(async (resolve, reject) => {
+    await googleMaps.places({
+        query: place,
+        location: [lat, long],
+        radius: 5000
+      })
+      .asPromise()
+      .then(function (response) {
+        let filteredResults = [];
+        for (let i = 0; i < response.json.results.length; i++) {
+          found = false;
+          for (let j = 0; j < filteredResults.length; j++) {
+            if (filteredResults[j].name == response.json.results[i].name) {
+              found = true;
+            }
+          }
+          if (!found)
+            filteredResults.push(response.json.results[i]);
+        }
+
+        resolve(filteredResults);
+        //   for (let i = 0; i < filteredResults.length; i++) {
+        //     console.log(filteredResults[i].name);
+        //   }
+        //   console.log(filteredResults.length);
+
+      });
+  });
+}
 
 
-
-// async function getNearbyFood(string, lat, long) {
-googleMaps.places({
-    query: string,
-    location: pos
-  }, function (result) {
-    console.log(result);
-  }).asPromise()
-  .then(function (response) {
-    console.log(response.json.results);
-  })
-// }
-
-
-
-// }
-
-// module.exports = {
-//   getNearbyFood: getNearbyFood
-// }
+module.exports = {
+  callPlacesAPI: callPlacesAPI
+}
